068b05691b39482d20647b8034bbbc0615a63939
stenmr <stenmr@tlu.ee>
2020-06-10 11:34:27 +0300

    Lae <head> indexist

--- index.php
+++ index.php
@@ -7,3 +7,13 @@ Flight::route('/', function () {
 
 Flight::start()
 ?>
+
+<!DOCTYPE html>
+<html lang="et">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script src="https://apis.google.com/js/platform.js" async defer></script>
+    <title>TLÜ OTT</title>
+</head>
--- views/home.php
+++ views/home.php
@@ -1,11 +1,3 @@
-<!DOCTYPE html>
-<html lang="et">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <title>TLÜ OTT</title>
-</head>
 <body>
     <nav>
         <h1>Tott</h1>
