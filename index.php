263ce3c5ca3c7ea433d6706e9a88ddc55061057e
stenmr <stenmr@tlu.ee>
2020-06-10 10:15:38 +0300

    Näidis HTML

--- index.php
+++ index.php
@@ -5,4 +5,54 @@ Flight::route('/', function () {
     echo 'tott!!!!';
 });
 
-Flight::start();
+Flight::start()
+?>
+
+<!DOCTYPE html>
+<html lang="et">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TLÜ OTT</title>
+</head>
+<body>
+    <nav>
+        <h1>Tott</h1>
+        <a href="login">Login</a>
+    </nav>
+
+    <aside>
+        <div class="category">Üks</div>
+        <div class="category">Teine</div>
+        <div class="category">Kolm</div>
+    </aside>
+
+    <div class="product-card">
+        <img src="https://via.placeholder.com/150.png" alt="pilt">
+        <h3>Kartul</h3>
+        <div class="amount-container">
+            <div class="minus"></div>
+            <output class="amount"></output>
+            <div class="plus"></div>
+        </div>
+    </div>
+    <div class="product-card">
+        <img src="https://via.placeholder.com/150.png" alt="pilt">
+        <h3>Maasikas</h3>
+        <div class="amount-container">
+            <div class="minus"></div>
+            <output class="amount"></output>
+            <div class="plus"></div>
+        </div>
+    </div>
+    <div class="product-card">
+        <img src="https://via.placeholder.com/150.png" alt="pilt">
+        <h3>Tomat</h3>
+        <div class="amount-container">
+            <div class="minus"></div>
+            <output class="amount"></output>
+            <div class="plus"></div>
+        </div>
+    </div>
+</body>
+</html>
