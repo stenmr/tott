98bca01cffd69169739420ef19ac57e2603b6a08
britli <britli@tlu.ee>
2020-06-10 11:56:13 +0300

    ksaj

--- views/home.php
+++ views/home.php
@@ -1,6 +1,18 @@
+<!DOCTYPE html>
+<html lang="et">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="assets/css/styles.css">
+    <title>TLÜ OTT</title>
+    <link rel="shortcut icon" href="../assets/logo.png" type="image/x-icon"/>
+</head>
+
 <body>
     <nav>
+        <img id="logo" src="../assets/logo.png" alt="Logo" class="logo">
         <h1>TLÜ OTT</h1>
+        <input type="text" id="search" maxlength="30">
         <a href="login">Login</a>
     </nav>
 
@@ -14,37 +26,37 @@
         <div class="category">Seemned ja teraviljad</div>
         <div class="category">Marjad</div>
         <div class="category">Liha ja kala</div>
-        
     </aside>
 
     <div class="products">
-    <div class="product-card">
-        <img src="https://via.placeholder.com/150.png" alt="pilt">
-        <h3>Kartul</h3>
-        <div class="amount-container">
-            <div class="minus"></div>
-            <output class="amount"></output>
-            <div class="plus"></div>
+        <div class="product-card">
+            <img src="https://via.placeholder.com/150.png" alt="pilt">
+            <h3>Kartuläöüõ</h3>
+            <div class="amount-container">
+                <div class="minus"></div>
+                <output class="amount"></output>
+                <div class="plus"></div>
+            </div>
+        </div>
+        <div class="product-card">
+            <img src="https://via.placeholder.com/150.png" alt="pilt">
+            <h3>Maasikas</h3>
+            <div class="amount-container">
+                <div class="minus"></div>
+                <output class="amount"></output>
+                <div class="plus"></div>
+            </div>
+        </div>
+        <div class="product-card">
+            <img src="https://via.placeholder.com/150.png" alt="pilt">
+            <h3>Tomat</h3>
+            <div class="amount-container">
+                <div class="minus"></div>
+                <output class="amount"></output>
+                <div class="plus"></div>
+            </div>
         </div>
     </div>
-    <div class="product-card">
-        <img src="https://via.placeholder.com/150.png" alt="pilt">
-        <h3>Maasikas</h3>
-        <div class="amount-container">
-            <div class="minus"></div>
-            <output class="amount"></output>
-            <div class="plus"></div>
-        </div>
-    </div>
-    <div class="product-card">
-        <img src="https://via.placeholder.com/150.png" alt="pilt">
-        <h3>Tomat</h3>
-        <div class="amount-container">
-            <div class="minus"></div>
-            <output class="amount"></output>
-            <div class="plus"></div>
-        </div>
-    </div>
-    </div>
+
 </body>
 </html>
