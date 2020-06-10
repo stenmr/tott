6e174ec270a47844972785757613e4214090d36c
stenmr <stenmr@tlu.ee>
2020-06-10 13:17:58 +0300

    Disainimuudatused

--- views/home.php
+++ views/home.php
@@ -12,11 +12,18 @@
     <nav>
         <img id="logo" src="../assets/logo.png" alt="Logo" class="logo">
         <h1>TLÜ OTT</h1>
-        <input type="text" id="search" maxlength="30">
-        <a href="login">Login</a>
+		<h4>Talutoit otse tootjalt Tallinna Ülikooli</h4>
     </nav>
+	
+	<div class="search">
+		<input type="text" id="search" maxlength="30">
+	</div>
+	
+	<div class="login">
+		<a href="login">Logi sisse</a>
+	</div>
 
-    <aside>
+    <aside class="category_box">
         <div class="category">Piimatooted ja munad</div>
         <div class="category">Puuviljad</div>
         <div class="category">Küpsetised</div>
@@ -24,14 +31,13 @@
         <div class="category">Hoidised</div>
         <div class="category">Joogid</div>
         <div class="category">Seemned ja teraviljad</div>
-        <div class="category">Marjad</div>
         <div class="category">Liha ja kala</div>
     </aside>
 
     <div class="products">
         <div class="product-card">
             <img src="https://via.placeholder.com/150.png" alt="pilt">
-            <h3>Kartuläöüõ</h3>
+            <h3>Kartulid</h3>
             <div class="amount-container">
                 <div class="minus"></div>
                 <output class="amount"></output>
