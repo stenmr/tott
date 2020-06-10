7648f7ced35316ea38c23c13d55ad50f00eb6fd5
elinasirk <esirk@tlu.ee>
2020-06-10 10:38:18 +0300

    Kategooriad kirjas 2

--- index.php
+++ index.php
@@ -22,9 +22,16 @@ Flight::start()
     </nav>
 
     <aside>
-        <div class="category">Üks</div>
-        <div class="category">Teine</div>
-        <div class="category">Kolm</div>
+        <div class="category">Piimatooted ja munad</div>
+        <div class="category">Puuviljad</div>
+        <div class="category">Küpsetised</div>
+        <div class="category">Juurviljad ja köögiviljad</div>
+        <div class="category">Hoidised</div>
+        <div class="category">Joogid</div>
+        <div class="category">Seemned ja teraviljad</div>
+        <div class="category">Marjad</div>
+        <div class="category">Liha ja kala</div>
+        
     </aside>
 
     <div class="product-card">
