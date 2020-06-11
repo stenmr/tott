1b2d8ba7a6f70a724d0f1de58a3c27cc6d296e98
stenmr <stenmr@tlu.ee>
2020-06-11 21:51:11 +0300

    Parem kaartide paigutus

--- views/home.php
+++ views/home.php
@@ -36,28 +36,83 @@
             </div>
             <div class="product-card">
                 <img src="https://via.placeholder.com/150.png" alt="pilt">
-                <h3>Maasikas</h3>
+                <h3>Kartulid</h3>
+                <div class="price">Siia tuleb hind</div>
                 <div class="amount-container">
                     <div class="minus">
                         <?php $icons->get('minus');?>
                     </div>
-                    <output class="amount"></output>
+                    <input type="text" id="amount" class="amount" value="0" min="0.5" step="0.5">
                     <div class="plus">
                         <?php $icons->get('plus');?>
                     </div>
                 </div>
-            </div>
-            <div class="product-card">
+                <div class="add-to-cart">
+                    <?php $icons->get('shopping-cart'); ?>
+                </div>
+            </div><div class="product-card">
                 <img src="https://via.placeholder.com/150.png" alt="pilt">
-                <h3>Tomat</h3>
+                <h3>Kartulid</h3>
+                <div class="price">Siia tuleb hind</div>
                 <div class="amount-container">
                     <div class="minus">
                         <?php $icons->get('minus');?>
                     </div>
-                    <output class="amount"></output>
+                    <input type="text" id="amount" class="amount" value="0" min="0.5" step="0.5">
                     <div class="plus">
                         <?php $icons->get('plus');?>
                     </div>
+                </div>
+                <div class="add-to-cart">
+                    <?php $icons->get('shopping-cart'); ?>
+                </div>
+            </div><div class="product-card">
+                <img src="https://via.placeholder.com/150.png" alt="pilt">
+                <h3>Kartulid</h3>
+                <div class="price">Siia tuleb hind</div>
+                <div class="amount-container">
+                    <div class="minus">
+                        <?php $icons->get('minus');?>
+                    </div>
+                    <input type="text" id="amount" class="amount" value="0" min="0.5" step="0.5">
+                    <div class="plus">
+                        <?php $icons->get('plus');?>
+                    </div>
+                </div>
+                <div class="add-to-cart">
+                    <?php $icons->get('shopping-cart'); ?>
+                </div>
+            </div><div class="product-card">
+                <img src="https://via.placeholder.com/150.png" alt="pilt">
+                <h3>Kartulid</h3>
+                <div class="price">Siia tuleb hind</div>
+                <div class="amount-container">
+                    <div class="minus">
+                        <?php $icons->get('minus');?>
+                    </div>
+                    <input type="text" id="amount" class="amount" value="0" min="0.5" step="0.5">
+                    <div class="plus">
+                        <?php $icons->get('plus');?>
+                    </div>
+                </div>
+                <div class="add-to-cart">
+                    <?php $icons->get('shopping-cart'); ?>
+                </div>
+            </div><div class="product-card">
+                <img src="https://via.placeholder.com/150.png" alt="pilt">
+                <h3>Kartulid</h3>
+                <div class="price">Siia tuleb hind</div>
+                <div class="amount-container">
+                    <div class="minus">
+                        <?php $icons->get('minus');?>
+                    </div>
+                    <input type="text" id="amount" class="amount" value="0" min="0.5" step="0.5">
+                    <div class="plus">
+                        <?php $icons->get('plus');?>
+                    </div>
+                </div>
+                <div class="add-to-cart">
+                    <?php $icons->get('shopping-cart'); ?>
                 </div>
             </div>
         </div>
