a7c62c28faf70094dcbbd8b45ff0aef482a04c63
stenmr <stenmr@tlu.ee>
2020-06-12 15:49:16 +0300

    Lisab selle checkbox hackiga FAQ

---
+++ views/faq.php
@@ -0,0 +1,26 @@
+<head>
+    <link rel="stylesheet" type="text/css" href="../assets/css/faq.css">
+</head>
+<body>
+    <div>
+        <input type="checkbox" id="faq-1">
+        <h2><label for="faq-1">Mis on TLÜ OTT eesmärk</label></h2>
+        <p>Välja töötada süsteem, mis võimaldab väiketootjate ja talunike kauba pakkumist Tallinna Ülikoolis ning
+            tarbijate teadlikkuse tõstmine, et eelistataks kohaliku toidu tarbimist.</p>
+    </div>
+
+    <div>
+        <input type="checkbox" id="faq-2">
+        <h2><label for="faq-2">Kuidas töötab talumehe tinder?</label></h2>
+        <p>TLU OTT pakub eestimaist talutoodangut. Kuna paljudel inimestel ei oma väga vahet mis talust toodang nende
+            lauale satub, vaid pigem on oluline selle kvaliteet. Sellega teeme meie inimeste elu kergemaks, sest meie
+            süsteemis on vaja valida ainult toode ja meie süsteem genereerib ise Teile talu kus toode Teieni jõuab.</p>
+    </div>
+
+    <div>
+        <input type="checkbox" id="faq-3">
+        <h2><label for="faq-3">Kuidas saan tellitud tooted kätte?</label></h2>
+        <p>Oma tellitud tooted saad kätte Tallinna Ülikoolis asuvas TLÜ OTT-i kapist, kus talunikud tooted tarnivad.
+            Meilile saabub ka teade kui tooted on kättesaamiseks kohal.</p>
+    </div>
+</body>
