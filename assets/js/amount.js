3358d20c17de94ea023848c11dc064d021f7c4b3
stenmr <stenmr@tlu.ee>
2020-06-16 10:50:39 +0300

    Sama katkine amount.js aga uuesti kirjutatud

--- assets/js/amount.js
+++ assets/js/amount.js
@@ -1,33 +1,12 @@
-
-
-$('.minus').on('click', function(e) {
-    e.preventDefault();
-    let $this = $(this);
-    let $input = $this.closest('div').find('input');
-    let value = parseInt($input.val());
-
-    if (value > 1) {
-        value = value - 1;
-    } else {
-        value = 0;
+for (const item of [document.getElementsByClassName('minus'), document.getElementsByClassName('plus')]) {
+    for (const element of item) {
+        element.addEventListener('click', function() {
+            const input = Array.from(this.parentElement.childNodes).find(x => x.tagName == 'INPUT');
+            if (Array.from(this.classList).includes('plus')) {
+                input.value = Number(input.value) + 1;
+            } else {
+                if (input.value > 0) input.value = Number(input.value) - 1;
+            }
+        })
     }
-
-$input.val(value);
-
-});
-
-$('.plus').on('click', function(e) {
-    e.preventDefault();
-    let $this = $(this);
-    let $input = $this.closest('div').find('input');
-    let value = parseInt($input.val());
-
-    if (value < 100) {
-      value = value + 1;
-    } else {
-        value =100;
-    }
-
-    $input.val(value);
-});
-
+}
