b6f0d87fbb00227b1746fc0d2f7f4564c7af27f2
stenmr <stenmr@tlu.ee>
2020-06-16 20:35:50 +0300

    Vormistus

--- views/navbar.php
+++ views/navbar.php
@@ -1,15 +1,19 @@
-<?php $icons = new Feather\Icons; ?>
+<?php $icons = new Feather\Icons;?>
 <head>
     <link rel="stylesheet" type="text/css" href="../assets/css/nav.css">
 </head>
 <nav>
-    <a href="/"><img src="../assets/logo.png" alt="Logo" class="logo navlink"><a href="/">
-    <a href="/" class="navlink"><h1>TLÜ OTT</h1></a>
-    <div class="basket"> 
-    <a href="/ostukorv"></a>
-    <?php $icons->get('shopping-cart');?>
-    Ostukorv
+    <a href="/"><img src="../assets/logo.png" alt="Logo" class="logo navlink"></a>
+    <a href="/" class="navlink">
+        <h1>TLÜ OTT</h1>
+    </a>
+    <div class="basket">
+        <a href="/ostukorv">
+            <?php $icons->get('shopping-cart');?>
+            Ostukorv
+        </a>
     </div>
 
-    <div class="g-signin2" data-width="240" data-height="50" data-longtitle="true" data-onsuccess="onSuccess" data-onfailure="onFailure"></div>
+    <div class="g-signin2" data-width="240" data-height="50" data-longtitle="true" data-onsuccess="onSuccess"
+        data-onfailure="onFailure"></div>
 </nav>
