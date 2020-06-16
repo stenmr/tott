837a48aec416917fa9f7f8694b925ec2497d83c2
britli <britli@tlu.ee>
2020-06-16 10:13:52 +0300

    cart

---
+++ views/cart.php
@@ -0,0 +1,11 @@
+<div class="shopping cart">
+    <div class="item">
+        <div class="delete button"></div>
+        <div class="image"> <img src="https://via.placeholder.com/600x400.jpg" alt="pilt"> </div>
+        <div class="name"><?php echo $name ?></div>
+        <div class="quantity"> </div>
+        <div class="full-price"></div>
+    </div>
+    <div class="total-price"></div>
+    <input type="submit" value="Esita tellimus" class="btn">
+</div>
