895ae87d3512a96b13912e23ff7f9565fa58120f
stenmr <stenmr@tlu.ee>
2020-06-16 14:56:22 +0300

    Eemalda otsing

--- views/navbar.php
+++ views/navbar.php
@@ -5,7 +5,5 @@
     <a href="/"><img src="../assets/logo.png" alt="Logo" class="logo navlink"><a href="/">
     <a href="/" class="navlink"><h1>TLÜ OTT</h1></a>
 
-    <input class="search" type="text" maxlength="30">
-
     <div class="g-signin2" data-width="240" data-height="50" data-longtitle="true" data-onsuccess="onSuccess" data-onfailure="onFailure"></div>
 </nav>
