e9daf3b68e37b0955d01e5cba62174435e6d6946
britli <britli@tlu.ee>
2020-06-16 09:39:24 +0300

    cards

---
+++ assets/js/amount.js
@@ -0,0 +1,33 @@
+
+
+$('.minus').on('click', function(e) {
+    e.preventDefault();
+    let $this = $(this);
+    let $input = $this.closest('div').find('input');
+    let value = parseInt($input.val());
+
+    if (value > 1) {
+        value = value - 1;
+    } else {
+        value = 0;
+    }
+
+$input.val(value);
+
+});
+
+$('.plus').on('click', function(e) {
+    e.preventDefault();
+    let $this = $(this);
+    let $input = $this.closest('div').find('input');
+    let value = parseInt($input.val());
+
+    if (value < 100) {
+      value = value + 1;
+    } else {
+        value =100;
+    }
+
+    $input.val(value);
+});
+
