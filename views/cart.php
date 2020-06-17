700f68679d9e46b184bc09ce8e36df1c1bb35ec7
elinasirk <esirk@tlu.ee>
2020-06-17 21:17:27 +0300

    Ostukorvi css algus

--- views/cart.php
+++ views/cart.php
@@ -1,11 +1,30 @@
+<?php $icons = new Feather\Icons; ?>
+<head>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="../assets/css/cart.css">
+</head>
+<body>
 <div class="shopping cart">
     <div class="item">
-        <div class="delete button"></div>
-        <div class="image"> <img src="https://via.placeholder.com/600x400.jpg" alt="pilt"> </div>
-        <div class="name"><?php //echo $name ?></div>
-        <div class="quantity"> </div>
-        <div class="full-price"></div>
+        <div class="name">Porgand</div>
+        <div class="quantity">2 kg</div>
+        <div class="full-price">2.50 €</div>
+        <div class="delete button"><i class="fa fa-trash w3-large"></i></div>
     </div>
-    <div class="total-price"></div>
-    <a href="/maksma"><input type="submit" value="Esita tellimus" class="btn"></a>
+    <div class="item">
+        <div class="name">Õun</div>
+        <div class="quantity">1.5 kg</div>
+        <div class="full-price">2.60 €</div>
+        <div class="delete button"><i class="fa fa-trash w3-large"></i></div>
+    </div>
+    <div class="item">
+        <div class="name">Kartul</div>
+        <div class="quantity">2 kg</div>
+        <div class="full-price">1.50 €</div>
+        <div class="delete button"><i class="fa fa-trash"></i></div>
+    </div>
+</div>
+<div class="finalcart">
+    <div class="total-price">Summa kokku</div>
+    <a href="/maksma"><input type="submit" value="Esita tellimus" class="button"></a>
 </div>
