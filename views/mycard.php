1b05b488e7523f31c9a556e75d36c50899f27cce
elinasirk <esirk@tlu.ee>
2020-06-17 12:58:10 +0300

    Veel taluniku kaarte

--- views/mycard.php
+++ views/mycard.php
@@ -8,12 +8,23 @@
     }  
 ?>
 
+<?php
+    $typeUnit;
+    if($type == 1){
+        $typeUnit =  "KG";
+    }
+    if($type == 0){
+        $typeUnit = "TK";
+    }  
+?>
+
 <div class="product-card">
     <img src="https://via.placeholder.com/600x400.jpg" alt="pilt">
     <h3><?php echo $name ?></h3>
     <div class="price"><?php echo $price . $typeText ?></div>
-    <div class="amount-container">
-        <input type="text" class="amount" value="1" min="1" step="1">
+    <div class="amount-container2">
+        <input type="text" class="myamount" value="1" min="1" step="1">
+        <?php echo $typeUnit ?>
     </div>
     <button type="button">Salvesta muudatused</button>
 </div>
