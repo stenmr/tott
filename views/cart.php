b41306cdeee927ffea31b1590477e0f9155dd912
elinasirk <esirk@tlu.ee>
2020-06-18 11:51:19 +0300

    Veelgi ostukorvi css ja asja

--- views/cart.php
+++ views/cart.php
@@ -3,27 +3,31 @@
 <link rel="stylesheet" type="text/css" href="../assets/css/cart.css">
 </head>
 <body>
-<div class="shopping cart">
+<section class="items_basket">
     <div class="item">
-        <div class="name">Porgand</div>
-        <div class="quantity">2 kg</div>
-        <div class="full-price">2.50 €</div>
-        <div class="delete button"><?php $icons->get('trash-2');?></div>
+        <div class="name item_part">Porgand</div>
+        <div class="quantity item_part">2 kg</div>
+        <div class="full-price item_part">2.50 €</div>
+        <div class="delete button item_part"><?php $icons->get('trash-2');?></div>
     </div>
     <div class="item">
-        <div class="name">Õun</div>
-        <div class="quantity">1.5 kg</div>
-        <div class="full-price">2.60 €</div>
-        <div class="delete button"><?php $icons->get('trash-2');?></div>
+        <div class="name item_part">Õun</div>
+        <div class="quantity item_part">1.5 kg</div>
+        <div class="full-price item_part">2.60 €</div>
+        <div class="delete button item_part"><?php $icons->get('trash-2');?></div>
     </div>
     <div class="item">
-        <div class="name">Kartul</div>
-        <div class="quantity">2 kg</div>
-        <div class="full-price">1.50 €</div>
-        <div class="delete button"><?php $icons->get('trash-2');?></div>
+        <div class="name item_part">Kartul</div>
+        <div class="quantity item_part">2 kg</div>
+        <div class="full-price item_part">1.50 €</div>
+        <div class="delete button item_part"><?php $icons->get('trash-2');?></div>
     </div>
-</div>
-<div class="finalcart">
+</section>
+<section class="finalcart">
     <div class="total-price">Summa kokku</div>
-    <a href="/maksma"><input type="submit" value="Esita tellimus" class="button submit_order"></a>
+    <div class="submit_button">
+    <a href="/maksma" class="submit_link">
+        <button>Maksma</button>
+    </a>
+</section>
 </div>
