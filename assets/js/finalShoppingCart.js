3906f616bc79c6f6a721e3291b7f3c8514103b08
britli <britli@tlu.ee>
2020-06-18 11:36:38 +0300

    shoppingcart

--- assets/js/finalShoppingCart.js
+++ assets/js/finalShoppingCart.js
@@ -5,6 +5,23 @@ if (!sessionStorage.hasItem('shopping-cart')) {
 const storage = sessionStorage.getItem('shopping-cart');
 const shoppingCartMap = new Map(JSON.parse(storage));
 
+let cart = document.getElementsByClassName('shopping-cart');
+
+
 for (const [id, stuff] of shoppingCartMap.entries()) {
+    let cardDiv = document.createElement('div');
+    let farmName = document.createElement('farmname');
+    let amount = document.createElement('amount');
+    let unitPrice = document.createElement('unitPrice')
+
+    cardDiv.value = stuff.name;
+    farmName.value = stuff.farmName;
+    amount.value = stuff.amount;
+    unitPrice.value = stuff.unitPrice;
+
+
+    cardDiv.appendChild(farmName);
+    cardDiv.appendChild(amount);
+    cardDiv.appendChild(unitPrice);
 
 }
